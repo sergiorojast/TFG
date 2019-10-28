4ec7eb36e830b4955d352f6b8fbe988ee46f1c8a
Sergio Rojas <sergiorojastorres2@gmail.com>
2019-10-28 08:10:22 +0100

    Añadido estructura cpanel
    Se ha añadido la estructura para el panel de control, donde estarán los crud, a su vez , se han creado los ficheros js y css correspondientes.

---
+++ Gesproj/js/controladorCpanel.js
@@ -0,0 +1,9 @@
+window.addEventListener('load',function(){
+    $('#btnComprimir').on('click',function(){
+        console.log('dentro');
+        
+        $("#barraLateral").animate({marginBottom:"150px"},3000);
+    })
+   
+})
+  
