300ee934a4dcbd87f9be77c1ce822f5ccff9b582
Sergio Rojas <sergiorojastorres2@gmail.com>
2019-11-02 21:03:49 +0100

    Añadida funcionalidad parcial a Cpanel
    Se acaba de añadir funcionalidad parcial al cpanel + imagenes

--- Gesproj/js/controladorCpanel.js
+++ Gesproj/js/controladorCpanel.js
@@ -1,9 +1,32 @@
 window.addEventListener('load',function(){
-    $('#btnComprimir').on('click',function(){
-        console.log('dentro');
-        
-        $("#barraLateral").animate({marginBottom:"150px"},3000);
+    //variable con la que controlaremos si la barra lateral esta colapsada o no.
+    var estadoBarraLateral =true;
+      
+   
+
+    $('#btnComprimir').click(function(){
+
+      if(estadoBarraLateral){
+        $('#barraLateral').animate({
+            width : '5%'
+        },300)
+        $('#iconoComprimir').attr('class','fa fa-chevron-right');
+
+        $('svg[data-toggle="tooltip"]').tooltip();
+      
+        estadoBarraLateral = !estadoBarraLateral;
+
+      }else{
+        $('#barraLateral').animate({
+            width : '10%'
+        },300)
+        $('#iconoComprimir').attr('class','fa fa-chevron-left')
+        estadoBarraLateral = !estadoBarraLateral;
+        $('svg[data-toggle="tooltip"]').tooltip('dispose');
+      }
+      $("small[id='textoBarraLateral']").toggle('fast');
     })
+
    
 })
   
