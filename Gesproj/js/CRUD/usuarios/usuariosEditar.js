723fb0a7fefe9147cd802177b9940399520fe086
Sergio Rojas <sergiorojastorres2@gmail.com>
2019-11-11 17:23:31 +0100

    Añadido input range para seleccionar los permisos en la edicion

--- Gesproj/js/CRUD/usuarios/usuariosEditar.js
+++ Gesproj/js/CRUD/usuarios/usuariosEditar.js
@@ -25,24 +25,33 @@ $(function () {
         })
         .done(function (datos) {
             let usuario = JSON.parse(datos)[0];
-            //console.log(usuario)
+            console.log(usuario)
             cargarDatosVista(usuario);
 
         })
         .fail(function (datos) {
             falloAjax();
         });
+
+
+        $('#eRange').mousemove(function(){
+            $('#numeroPermisos').html($(this).val());
+           // console.log($(this).val())
+        })
 })
 
 
 function cargarDatosVista(u) {
-    $('#dCorreo').val(u.correo);
-    $('#dNombre').val(u.nombre);
-    $('#dApellidos').val(u.apellidos);
+    $('#eCorreo').val(u.correo);
+    $('#eNombre').val(u.nombre);
+    $('#eApellidos').val(u.apellidos);
+   
 
     $('#imagen').attr('src', repositorioImagenes + "/" + u.imagen);
 
-
+    $('.rango').html("<label for='customRange2'>Rol <small> nivel de permisos <span id='numeroPermisos'></span></small><span class='fab fa-keycdn'></span></label>"+
+    "<input type='range' class='custom-range' min='0' max='100' value='"+u.rol+"' id='eRange'>");
+    $('#eRange').attr('value',u.rol);
 
 }
 
