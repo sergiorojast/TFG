1d05ab76290c28a860d55308dbf7a30d8890a629
Sergio Rojas <sergiorojastorres2@gmail.com>
2019-11-11 15:45:22 +0100

    Creado formulario Editar
    Se acaba de crear el formulario para la edición de los usuarios

---
+++ Gesproj/js/CRUD/usuarios/usuariosEditar.js
@@ -0,0 +1,60 @@
+$(function () {
+
+
+
+    $('#volverResumenUsuario').click(botonUsuarios);
+
+
+    if (correo == undefined || correo == "") {
+        bootbox.alert({
+            message: "Fallo al extraer el correo de la tabla",
+            backdrop: true
+        })
+        botonUsuarios();
+    }
+
+
+
+    $.ajax({
+            type: "POST",
+            url: webService,
+            data: {
+                'accion': 'visualizarUsuario',
+                'correo': correo
+            },
+        })
+        .done(function (datos) {
+            let usuario = JSON.parse(datos)[0];
+            //console.log(usuario)
+            cargarDatosVista(usuario);
+
+        })
+        .fail(function (datos) {
+            falloAjax();
+        });
+})
+
+
+function cargarDatosVista(u) {
+    $('#dCorreo').val(u.correo);
+    $('#dNombre').val(u.nombre);
+    $('#dApellidos').val(u.apellidos);
+
+    $('#imagen').attr('src', repositorioImagenes + "/" + u.imagen);
+
+
+
+}
+
+
+
+function botonUsuarios() {
+    $('#contenido').empty();
+    $('#contenido').html(preload);
+
+
+    $.post('vistas/usuarios/usuarios.html', function (htmle) {
+        $('#contenido').html(htmle);
+    }, 'html');
+
+}
