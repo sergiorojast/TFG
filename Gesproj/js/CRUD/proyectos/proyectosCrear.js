ef6d7a2a9f2a7f7a0754dcb3b91f09aae473c007
Sergio Rojas <sergiorojastorres2@gmail.com>
2019-11-26 13:55:59 +0100

    Creada la estructura basica del formulario de creacion de proyectos

--- Gesproj/js/CRUD/proyectos/proyectosCrear.js
+++ Gesproj/js/CRUD/proyectos/proyectosCrear.js
@@ -1,9 +1,17 @@
-
-$(function(){
+$(function () {
 
     agregarFuncionalidadBotonesRetroceder();
 
-  
+    validarFormulario();
+
+    //Añadimos  el envio del formulario  con el evento 
+    $('#botonCrear').click(function () {
+        $('#formularioProyecto').submit();
+    })
+
+    solicitarDatosAdministradores();
+
+    agrearFuncionalidadSelectorAdministradores();
 })
 
 
@@ -11,21 +19,117 @@ $(function(){
 /**
  * procedimiento encargado de añadir los eventos click a los botones de volver atras.
  */
-function agregarFuncionalidadBotonesRetroceder(){
-        //Añadimos  evento y "redireccion" para volver al listado de proyectos
-        $('#volverResumenProyectos').click(volverAtras);
-        $('#botonAtras').click(volverAtras);
+function agregarFuncionalidadBotonesRetroceder() {
+    //Añadimos  evento y "redireccion" para volver al listado de proyectos
+    $('#volverResumenProyectos').click(volverAtras);
+    $('#botonAtras').click(volverAtras);
 }
 
 /**
  * procedimiento encargado de cambiar  la vista por el resumen de proyectos.
  */
-function volverAtras(){
+function volverAtras() {
     $('#contenido').empty();
     $('#contenido').html(preload);
 
 
-     $.post('vistas/proyectos/proyectosResumen.html', function (htmle) {
+    $.post('vistas/proyectos/proyectosResumen.html', function (htmle) {
         $('#contenido').html(htmle);
-     }, 'html');
+    }, 'html');
 }
+
+/**
+ * Funcion encargada de validar el formulario de envio de datos para crear el proyecto en la base de datos.
+ */
+
+function validarFormulario() {
+    $('#formularioProyecto').validate({
+        rules: {
+            'cNombreProyecto': {
+                required: true
+            },
+            'cDescripcionProyecto': {
+                required: true,
+                minlength: 3,
+                maxlength: 6000
+
+            }
+
+        },
+        errorElement: "small",
+        errorPlacement: function (error, element) {
+            // Add the `invalid-feedback` class to the error element
+            error.addClass("invalid-feedback");
+
+
+            if (element.prop("type") === "checkbox") {
+                error.insertAfter(element.next("label"));
+            } else {
+                error.insertAfter(element);
+
+            }
+        },
+        highlight: function (element, errorClass, validClass) {
+            $(element).addClass("is-invalid").removeClass("is-valid");
+        },
+        unhighlight: function (element, errorClass, validClass) {
+            $(element).addClass("is-valid").removeClass("is-invalid");
+        },
+        messages: {
+            'cNombreProyecto': {
+                required: "Dato requerido"
+            },
+            'cDescripcionProyecto': {
+                required: "Dato requerido",
+                maxlength: "Descripción demasiado larga, el texto que esta introduciendo como descripción es superior a 1000 palabras.",
+                minlength: "Descipción demasiado corta."
+            }
+        },
+
+
+        submitHandler: function (form, event) {
+            event.preventDefault();
+        }
+    })
+}
+
+function solicitarDatosAdministradores() {
+    $.ajax({
+            type: "POST",
+            url: webService,
+            data: {
+                'accion': 'listadoAdministradores'
+            },
+        })
+        .done(function (data) {
+
+            if (isNaN(data)) {
+                let datos = JSON.parse(data);
+
+                for (let i = 0; i < datos.length; i++) {
+                    $('#selectorAdministradores').append("<option value='" + datos[i] + "'> " + datos[i] + " </option>");
+                }
+            } else if (data === -1) {
+                mensajeDanger('Sesion no iniciada')
+            } else {
+                mensajeDanger('No tienes permisos para hacer esta acción');
+            }
+        })
+        .fail(function (data) {
+            falloAjax();
+        })
+}
+
+function agrearFuncionalidadSelectorAdministradores() {
+
+
+    $('#botonSeleccionarAdministradores').click(function () {
+            let correoSeleccionado = $('#selectorAdministradores').val();
+            if (correoSeleccionado !== 'Seleccionar Administradores...') {
+
+                $('#listadoAdministradores').append("<li value='" + correoSeleccionado + "'>" + correoSeleccionado + "</li>");
+                $("#selectorAdministradores option[value='" + correoSeleccionado + "']").remove();
+
+                }
+            })
+    }
